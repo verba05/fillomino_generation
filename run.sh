cab4b6de30df67c3606c8ee0faf83c03eb0f7a44
Ivan <verba447@gmail.com>
2026-09-18 23:22:54 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+xhost +local:docker
 docker build -t fillomino .
-docker run -it --rm     -e DISPLAY=$DISPLAY     -v /tmp/.X11-unix:/tmp/.X11-unix fillomino
+docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix fillomino > /dev/null
